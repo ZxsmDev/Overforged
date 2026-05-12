26bcaf640ddd7cc6693481ca9853b131d7a853b6
ZxsmDev <zxsm.dev@gmail.com>
2026-05-11 23:51:38 -0600

    Initial commit

---
+++ src/main/java/overforged/modid/OverforgedClient.java
@@ -0,0 +1,10 @@
+package overforged.modid;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class OverforgedClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
